94068cfd7294e4068c14894751d442d04eff0cb6
danhsakai <dohuudanh996@gmail.com>
2026-04-16 00:48:52 +0700

    feat: add responsive for tablet and mobile screen, add toggle menu with hamburger icon

--- src/main.js
+++ src/main.js
@@ -1,1 +1,10 @@
 import './style.css';
+const btnMenu = document.querySelector('.hamburger');
+const mobileMenu = document.querySelector('.menu__mobile');
+
+
+
+
+btnMenu.addEventListener('click', () => {
+  mobileMenu.classList.toggle('menu__mobile--active');
+});
